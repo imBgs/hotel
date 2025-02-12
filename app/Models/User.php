43a160881baa718d633f9f47bd76da396fd45f9e
imambagasadriyano <bagasadriyano@gmail.com>
2025-02-12 09:41:49 +0700

    FEAT[menambahkan profile picture, phone number]

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,11 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Enums\Role;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -21,8 +22,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'email_verified_at',
         'role',
-
+        'profile_photo_path',
+        'phone_number',
     ];
 
     /**
@@ -43,6 +46,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
-        'role'=> 'string',
+        'role'=> Role::class,
     ];
+
 }
